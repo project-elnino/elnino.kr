4412f1e93af36143f53d7b2fcd3695bb7a64f5f8
angjun04 <peteryoung04@naver.com>
2025-05-21 15:28:05 +0900

    replaced icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Elnino",
   description: "Realtime Translation Subtitle",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
